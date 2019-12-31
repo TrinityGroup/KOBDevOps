88dcb9286e6cc42e6452ea654f10ec33c0d4fb17
4thepeople <xx3091@gmail.com>
2019-12-31 14:46:39 +0530

    status command fix

--- src/main/bash/sh/kobman-status.sh
+++ src/main/bash/sh/kobman-status.sh
@@ -3,36 +3,37 @@
 
 function __kob_status {
 
-figlet memory upload of 
-figlet kobman-status.sh
-
-if [ -d "${KOBMAN_DIR}/env/von-network" ] 
-then
-	figlet tob von
-	figlet available		
-else
-	figlet tob von
-	figlet not-available		
-fi
-
-
-
-if [ -d "${KOBMAN_DIR}/env/TheOrgBook" ] 
-then
-	figlet tob 
-	figlet available		
-else
-	figlet tob 
-	figlet not-available		
-fi
-
-if [ -d "${KOBMAN_DIR}/env/greenlight" ]
-then
-	figlet tob greenlight 
-	figlet available		
-else
-	figlet greenlight  
-	figlet not-available		
-fi
-
+echo "print out the status from candidate folder"
+#	figlet memory upload of 
+#	figlet kobman-status.sh
+#
+#	if [ -d "${KOBMAN_DIR}/env/von-network" ] 
+#	then
+#		figlet tob von
+#		figlet available		
+#	else
+#		figlet tob von
+#		figlet not-available		
+#	fi
+#
+#
+#
+#	if [ -d "${KOBMAN_DIR}/env/TheOrgBook" ] 
+#	then
+#		figlet tob 
+#		figlet available		
+#	else
+#		figlet tob 
+#	figlet not-available		
+#	fi
+#
+#	if [ -d "${KOBMAN_DIR}/env/greenlight" ]
+#	then
+#		figlet tob greenlight 
+#		figlet available		
+#	else
+#		figlet greenlight  
+#		figlet not-available		
+#	fi
+#
 }
