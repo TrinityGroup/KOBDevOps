bcc1a3028e327719885a74056b00ee365dd280e3
4thepeople <xx3091@gmail.com>
2020-01-01 20:39:03 +0530

    from : sh To : commands

--- src/main/bash/automata_zip_create.sh
+++ src/main/bash/automata_zip_create.sh
@@ -2,7 +2,7 @@
 
 echo "Before..."
 ls -latr ../../../kobman_zip_file.zip
-sudo zip -rj ../../../kobman_zip_file.zip env/kobman-* sh/kobman-*
+sudo zip -rj ../../../kobman_zip_file.zip env/kobman-* commands/kobman-*
 echo "....after."
 ls -latr ../../../kobman_zip_file.zip
 
