d50356df805e5a39a5c313081e3253b14e537373
4thepeople <xx3091@gmail.com>
2020-01-01 20:39:48 +0530

    renaming sh directory to commands directory

---
+++ src/main/bash/commands/kobman-status.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+function __kob_status {
+
+echo "Kobman Environment Tree Structure"
+tree .kobman/candidates/
+
+# echo "env variable print"
+# tree ${KOBMAN_CANDIDATES_DIR}
+
+#	figlet memory upload of 
+#	figlet kobman-status.sh
+#
+#	if [ -d "${KOBMAN_DIR}/env/von-network" ] 
+#	then
+#		figlet tob von
+#		figlet available		
+#	else
+#		figlet tob von
+#		figlet not-available		
+#	fi
+#
+#
+#
+#	if [ -d "${KOBMAN_DIR}/env/TheOrgBook" ] 
+#	then
+#		figlet tob 
+#		figlet available		
+#	else
+#		figlet tob 
+#	figlet not-available		
+#	fi
+#
+#	if [ -d "${KOBMAN_DIR}/env/greenlight" ]
+#	then
+#		figlet tob greenlight 
+#		figlet available		
+#	else
+#		figlet greenlight  
+#		figlet not-available		
+#	fi
+#
+}
