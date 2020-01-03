e1d6f91ede19d9774d028d8c3a97993478ff38ac
4thepeople <xx3091@gmail.com>
2020-01-03 17:34:30 +0530

    fixing kob-list

--- src/main/bash/commands/kobman-list.sh
+++ src/main/bash/commands/kobman-list.sh
@@ -1,1 +1,6 @@
-		https://raw.githubusercontent.com/EtricKombat/KOBDevOps/master/list | less
+#!/bin/bash
+
+function __kob_list {
+https://raw.githubusercontent.com/EtricKombat/KOBDevOps/master/list | less
+
+}
