3ee72cb16f517c996b127b148e73982548212508
EtricKombat <33585301+EtricKombat@users.noreply.github.com>
2020-12-10 09:08:31 +0530

    Update ubuntu18_jfrog.sh

--- ubuntu18_jfrog.sh
+++ ubuntu18_jfrog.sh
@@ -3,8 +3,11 @@
 sudo apt install figlet -y
 sudo figlet JFrog Artifactory installation in ubuntu 18.04
 
-echo "Step 1 — ?
-echo "Step 2 — ?
-echo "Step 3 — ?
+echo "Step 1 — ?"
+echo "Step 2 — ?"
+echo "Step 3 — ?"
+
+
+echo "Ref : https://computingforgeeks.com/how-to-install-jfrog-artifactory-on-ubuntu/"
 
 
